fd446337beec02a8bcab752e529cfcc7bcc847ce
Mijail <mmijailm@hotmail.com>
2017-08-07 21:30:07 -0600

    Boton iniciar y navigation drawer

--- app/src/main/java/com/hxrsoft/vetnica/MainActivity.java
+++ app/src/main/java/com/hxrsoft/vetnica/MainActivity.java
@@ -1,13 +1,30 @@
 package com.hxrsoft.vetnica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button iniciar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iniciar=(Button)findViewById(R.id.iniciar);
+
+        iniciar.setOnClickListener(new View.OnClickListener()
+        {
+
+        public void onClick(View arg0){
+            Intent inten = new Intent(MainActivity.this,Veterinarias.class);
+            startActivity(inten);
+
+        }
+
+        });
+
     }
 }
