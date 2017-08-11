cc8b693e0fb1ccc6a35df14067d0ea1ec7c2e425
Angel A. Pineda Benard <haxorfox@nicafur.com>
2017-08-10 21:19:22 -0600

    Ubica Marcadores desde Firebase

--- app/src/main/java/com/hxrsoft/vetnica/MapsActivity.java
+++ app/src/main/java/com/hxrsoft/vetnica/MapsActivity.java
@@ -1,8 +1,18 @@
 package com.hxrsoft.vetnica;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.Manifest;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,61 +24,156 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import com.hxrsoft.vetnica.model.Local;
 
-    private static final String TAG = "LOG";
-    private GoogleMap mMap;
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,LocationListener,ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private LocationManager lm;
+    private Location location;
+    private double longitude;
+    private double latitude;
+
+    private FirebaseDatabase database;
+
+    private static final int REQUEST_PERMISSION = 1;
+
+    private GoogleMap map;
+
+
+    private static String[] PERMISSIONS = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("marcadores");
+        database = FirebaseDatabase.getInstance();
 
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        initMaps();
+
+
+
+    }
+
+    public void initMaps(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions();
+
+        } else {
+            lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 60000, this);
+        }
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        this.map = map;
+        if (lm != null) {
+            if (location != null) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+
+            }
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+        }
+        map.setTrafficEnabled(true);
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 11));
+
+        loadMarker();
+    }
+
+    @Override
+    public void onLocationChanged(Location arg0) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String arg0) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String arg0) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this,"Autorizado",Toast.LENGTH_SHORT).show();
+                    initMaps();
+
+                } else {
+                    Toast.makeText(this,"Permiso Denegado",Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+        }
+    }
+
+    private void requestPermissions() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                || ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+
+            Toast.makeText(this,"Permiso Denegado",Toast.LENGTH_SHORT).show();
+        } else {
+            ActivityCompat.requestPermissions(this,PERMISSIONS , REQUEST_PERMISSION);
+        }
+    }
+
+    public void loadMarker(){
+        DatabaseReference marcadores = database.getReference("marcadores");
+
+        marcadores.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> dataSnapshots = dataSnapshot.getChildren();
+                map.clear();
+                for (DataSnapshot dataSnapshot1 : dataSnapshots) {
+                    Local local = dataSnapshot1.getValue(Local.class);
+                    map.addMarker(new MarkerOptions()
+                            .position(new LatLng(local.getLatitude(), local.getLongitude()))
+                            .title(local.getNombre())
+                            .snippet(local.getDescripcion()));
+                }
             }
 
             @Override
             public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
 }
+
+
---
+++ app/src/main/java/com/hxrsoft/vetnica/model/Local.java
@@ -0,0 +1,57 @@
+package com.hxrsoft.vetnica.model;
+
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Local {
+
+    private String nombre;
+    private double latitude;
+    private double longitude;
+    private String descripcion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("nombre", nombre);
+        result.put("latitude", latitude);
+        result.put("longitude", longitude);
+        result.put("descripcion", descripcion);
+
+        return result;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
